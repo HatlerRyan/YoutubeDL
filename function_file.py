c8e8b4d4d9228b45ae185d384888c346572bb459
hatler <HatlerRyan@gmail.com>
2022-09-14 20:43:58 -0600

    can't make up my mind

---
+++ function_file.py
@@ -0,0 +1,19 @@
+from pytube import Channel, YouTube
+from tkinter import *
+
+
+class SomethingRandom:
+    def __init__(self, url_type):
+        self.url_type = url_type
+
+    def next_button_press(self, url_type):
+        return f'{self.url_type}'
+
+
+
+# def heres_where_the_fun_begins():
+#     if SomethingRandom.next_button_press == "video":
+#         video_window = Label()
+#         messagebox.askokcancel()
+
+
--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-
 from tkinter import *
-
+from function_file import SomethingRandom
 
 window = Tk()
 window.title("YoutubeDL")
 window.config(width=500, height=500, pady=20, padx=20)
 url_text = Label(text="URL of:")
 url_text.grid(column=0, row=0, columnspan=2)
+
+
+def selected():
+    radiobutton_value = channel_or_video.get()
+    return radiobutton_value
+
+def busted_face():
+    if selected == 'video':
+        messagebox.askquestion("sjkdflsj", "djfksljf")
+
+
 channel_or_video = StringVar(None, "video")
-channel = Radiobutton(text="Channel", variable=channel_or_video, value="channel")
-video = Radiobutton(text='Video', variable=channel_or_video, value="video")
+channel = Radiobutton(text="Channel", variable=channel_or_video, value="channel", command=selected)
+video = Radiobutton(text='Video', variable=channel_or_video, value="video", command=selected)
+
 channel.grid(column=0, row=1, sticky="e")
 video.grid(column=1, row=1, sticky="w")
 url_entry = Entry(width=30)
 url_entry.focus()
 
-url_entry.grid(column=0, row=4,columnspan=2)
+url_entry.grid(column=0, row=4, columnspan=2)
+# next_button_press = SomethingRandom.next_button_press(url_type="help")
+what_type = SomethingRandom(url_type=selected())
+# print(what_type)
 
-next_button = Button(text="Next",)
-next_button.grid(column=0,row=6, columnspan=2, pady=5)
+next_button = Button(text="Next", command=busted_face)
+next_button.grid(column=0, row=6, columnspan=2, pady=5)
+
 
 
 
