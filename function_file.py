2c18f2128cec5fa938a483e5d86232a4b7d91abf
hatler <HatlerRyan@gmail.com>
2022-09-16 17:24:41 -0600

    Added more messageboxes to guide users where to download

--- function_file.py
+++ function_file.py
@@ -6,7 +6,21 @@ def video_only(self):
     yt = YouTube(self)
     video_title = yt.title
     print(video_title)
-    messagebox.askyesno(title="video confirmation",
+    video_confirmation_mb = messagebox.askyesno(title="video confirmation",
                         message=f"Is this the video you want to download?\n"
                                 f"{video_title}")
+    video_confirmation_mb
+    if video_confirmation_mb:
+        video_audio_mb = messagebox.askyesno(title="video or audio",
+                            message="Do you want audio only?")
+        video_audio_mb
+        if video_audio_mb:
+            stream = yt.streams.get_by_itag(140)
+            music_podcast_mb = messagebox.askyesno(title="music or podcast",
+                            message="Is this music?")
+            music_podcast_mb
+            if music_podcast_mb:
+                stream.download('music_downloads')
+                print("You did it")
+
 
